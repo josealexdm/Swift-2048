be6567103c577dfa3e455d9ce5c6cf667eabc095
Jose Alexandre de Moraes <josealexdm@gmail.com>
2024-07-18 07:40:15 -0300

    First working version. Pending to work on UI and some code improvements

---
+++ 2048-Game/GameBrain.swift
@@ -0,0 +1,260 @@
+//
+//  GameBrain.swift
+//  2048-Game
+//
+//  Created by user262803 on 6/17/24.
+//
+
+import Foundation
+
+struct GameBrain {
+    var gameArray = [["", "", "", ""], ["", "", "", ""], ["", "", "", ""], ["", "", "", ""]]
+    var pointerArray = [0, 0, 0, 0]
+    var score = 0
+    
+    mutating func startNewGame() {
+        //gameArray = [["", "", "", ""], ["", "", "", ""], ["", "", "", ""], ["", "", "", ""]]
+        gameArray = [["2", "4", "8", "16"], ["16", "8", "4", "2"], ["2", "64", "8", "16"], ["1024", "", "", ""]]
+        score = 0
+        var count = 0
+        var row = 0
+        var col = 0
+        repeat {
+            row = Int.random(in: 0...3)
+            col = Int.random(in: 0...3)
+            if gameArray[row][col] == "" {
+                gameArray[row][col] = "2"
+                count += 1
+            }
+        } while count < 2
+    }
+
+    mutating func continueGame() {
+        var count = 0
+        var maxCount = 0
+        var row = 0
+        var col = 0
+        repeat {
+            row = Int.random(in: 0...3)
+            col = Int.random(in: 0...3)
+            if gameArray[row][col] == "" {
+                gameArray[row][col] = "2"
+                count += 1
+            }
+            // use maxCount to avoid infinite loop
+            maxCount += 1
+            //print("passou:\(maxCount)")
+        } while count < 1 && maxCount < 100
+    }
+
+    func checkGameOver() -> Bool {
+        for row in 0...3 {
+            for col in 0...3 {
+                if gameArray[row][col] == "" {
+                    // return false while there is at least one empty position
+                    return false
+                }
+                if row < 3 && col < 3 {
+                    // return false while there is at least one pair of values to be summarized
+                    if gameArray[row][col] == gameArray[row][col + 1] || gameArray[row][col] == gameArray[row + 1][col] {
+                        return false
+                    }
+                }
+                if row == 3 && col < 3 {
+                    // return false while there is at least one pair of values to be summarized
+                    if gameArray[row][col] == gameArray[row][col + 1] {
+                        return false
+                    }
+                }
+                if row < 3 && col == 3 {
+                    // return false while there is at least one pair of values to be summarized
+                    if gameArray[row][col] == gameArray[row + 1][col] {
+                        return false
+                    }
+                }
+            }
+        }
+        return true
+    }
+    
+    mutating func moveRight() {
+        var pointer = 0
+        var col = 0
+        var cellValue = 0
+        for row in 0...3 {
+            pointer = pointerArray[row]
+            //print(pointer)
+            if gameArray[row][pointer] == "" {
+                // Shift right when position is empty
+                col = pointer
+                while col > 0 {
+                    gameArray[row][col] = gameArray[row][col - 1]
+                    col -= 1
+                }
+                gameArray[row][0] = ""
+            } else {
+                if gameArray[row][pointer] == gameArray[row][pointer - 1] {
+                    // Sum and shift other columns when columns have same value
+                    cellValue = Int(gameArray[row][pointer])! * 2
+                    score += cellValue
+                    gameArray[row][pointer] = "\(cellValue)"
+                    col = pointer - 1
+                    while col > 0 {
+                        //print("col:\(col)")
+                        gameArray[row][col] = gameArray[row][col - 1]
+                        col -= 1
+                    }
+                    gameArray[row][0] = ""
+                    pointerArray[row] -= 1
+                } else if gameArray[row][pointer - 1] == "" {
+                    // Shift other columns when position at left side of pointer is empty
+                    col = pointer - 1
+                    while col > 0 {
+                        gameArray[row][col] = gameArray[row][col - 1]
+                        col -= 1
+                    }
+                    gameArray[row][0] = ""
+                } else {
+                    // Both columns are not empty but with different values, so only pointer need to be updated
+                    pointerArray[row] -= 1
+                }
+            }
+        }
+    }
+    
+    mutating func moveLeft() {
+        var pointer = 0
+        var col = 0
+        var cellValue = 0
+        for row in 0...3 {
+            pointer = pointerArray[row]
+            //print(pointer)
+            if gameArray[row][pointer] == "" {
+                // Shift left when position is empty
+                col = pointer
+                while col < 3 {
+                    gameArray[row][col] = gameArray[row][col + 1]
+                    col += 1
+                }
+                gameArray[row][3] = ""
+            } else {
+                if gameArray[row][pointer] == gameArray[row][pointer + 1] {
+                    // Sum and shift other columns when columns have same value
+                    cellValue = Int(gameArray[row][pointer])! * 2
+                    score += cellValue
+                    gameArray[row][pointer] = "\(cellValue)"
+                    col = pointer + 1
+                    while col < 3 {
+                        //print("col:\(col)")
+                        gameArray[row][col] = gameArray[row][col + 1]
+                        col += 1
+                    }
+                    gameArray[row][3] = ""
+                    pointerArray[row] += 1
+                } else if gameArray[row][pointer + 1] == "" {
+                    // Shift other columns when position at right side of pointer is empty
+                    col = pointer + 1
+                    while col < 3 {
+                        gameArray[row][col] = gameArray[row][col + 1]
+                        col += 1
+                    }
+                    gameArray[row][3] = ""
+                } else {
+                    // Both columns are not empty but with different values, so only pointer need to be updated
+                    pointerArray[row] += 1
+                }
+            }
+        }
+    }
+    
+    mutating func moveDown() {
+        var pointer = 0
+        var row = 0
+        var cellValue = 0
+        for col in 0...3 {
+            pointer = pointerArray[col]
+            //print(pointer)
+            if gameArray[pointer][col] == "" {
+                // Shift right when position is empty
+                row = pointer
+                while row > 0 {
+                    gameArray[row][col] = gameArray[row - 1][col]
+                    row -= 1
+                }
+                gameArray[0][col] = ""
+            } else {
+                if gameArray[pointer][col] == gameArray[pointer - 1][col] {
+                    // Sum and shift other columns when columns have same value
+                    cellValue = Int(gameArray[pointer][col])! * 2
+                    score += cellValue
+                    gameArray[pointer][col] = "\(cellValue)"
+                    row = pointer - 1
+                    while row > 0 {
+                        //print("col:\(col)")
+                        gameArray[row][col] = gameArray[row - 1][col]
+                        row -= 1
+                    }
+                    gameArray[0][col] = ""
+                    pointerArray[col] -= 1
+                } else if gameArray[pointer - 1][col] == "" {
+                    // Shift other columns when position at left side of pointer is empty
+                    row = pointer - 1
+                    while row > 0 {
+                        gameArray[row][col] = gameArray[row - 1][col]
+                        row -= 1
+                    }
+                    gameArray[0][col] = ""
+                } else {
+                    // Both columns are not empty but with different values, so only pointer need to be updated
+                    pointerArray[col] -= 1
+                }
+            }
+        }
+    }
+    
+    mutating func moveUp() {
+        var pointer = 0
+        var row = 0
+        var cellValue = 0
+        for col in 0...3 {
+            pointer = pointerArray[col]
+            //print(pointer)
+            if gameArray[pointer][col] == "" {
+                // Shift left when position is empty
+                row = pointer
+                while row < 3 {
+                    gameArray[row][col] = gameArray[row + 1][col]
+                    row += 1
+                }
+                gameArray[3][col] = ""
+            } else {
+                if gameArray[pointer][col] == gameArray[pointer + 1][col] {
+                    // Sum and shift other columns when columns have same value
+                    cellValue = Int(gameArray[pointer][col])! * 2
+                    score += cellValue
+                    gameArray[pointer][col] = "\(cellValue)"
+                    row = pointer + 1
+                    while row < 3 {
+                        //print("col:\(col)")
+                        gameArray[row][col] = gameArray[row + 1][col]
+                        row += 1
+                    }
+                    gameArray[3][col] = ""
+                    pointerArray[col] += 1
+                } else if gameArray[pointer + 1][col] == "" {
+                    // Shift other columns when position at right side of pointer is empty
+                    row = pointer + 1
+                    while row < 3 {
+                        gameArray[row][col] = gameArray[row + 1][col]
+                        row += 1
+                    }
+                    gameArray[3][col] = ""
+                } else {
+                    // Both columns are not empty but with different values, so only pointer need to be updated
+                    pointerArray[col] += 1
+                }
+            }
+        }
+    }
+
+}
---
+++ 2048-Game/ViewController.swift
@@ -0,0 +1,136 @@
+//
+//  ViewController.swift
+//  2048-Game
+//
+//  Created by user262803 on 6/17/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var gameBrain = GameBrain()
+
+    @IBOutlet var swipeRight: UISwipeGestureRecognizer!
+    @IBOutlet var swipeLeft: UISwipeGestureRecognizer!
+    @IBOutlet var swipeDown: UISwipeGestureRecognizer!
+    @IBOutlet var swipeUp: UISwipeGestureRecognizer!
+    @IBOutlet weak var cell00: UILabel!
+    @IBOutlet weak var cell01: UILabel!
+    @IBOutlet weak var cell02: UILabel!
+    @IBOutlet weak var cell03: UILabel!
+    @IBOutlet weak var cell10: UILabel!
+    @IBOutlet weak var cell11: UILabel!
+    @IBOutlet weak var cell12: UILabel!
+    @IBOutlet weak var cell13: UILabel!
+    @IBOutlet weak var cell20: UILabel!
+    @IBOutlet weak var cell21: UILabel!
+    @IBOutlet weak var cell22: UILabel!
+    @IBOutlet weak var cell23: UILabel!
+    @IBOutlet weak var cell30: UILabel!
+    @IBOutlet weak var cell31: UILabel!
+    @IBOutlet weak var cell32: UILabel!
+    @IBOutlet weak var cell33: UILabel!
+    @IBOutlet weak var labelScore: UILabel!
+    @IBOutlet weak var labelGameOver: UILabel!
+    @IBOutlet weak var buttonNewGame: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        gameBrain.startNewGame()
+        labelGameOver.isHidden = true
+        buttonNewGame.isHidden = true
+        updateCells()
+    }
+
+    func updateCells() {
+        // Read gameArray and update Labels
+        cell00.text = "\(gameBrain.gameArray[0][0])"
+        cell01.text = "\(gameBrain.gameArray[0][1])"
+        cell02.text = "\(gameBrain.gameArray[0][2])"
+        cell03.text = "\(gameBrain.gameArray[0][3])"
+        cell10.text = "\(gameBrain.gameArray[1][0])"
+        cell11.text = "\(gameBrain.gameArray[1][1])"
+        cell12.text = "\(gameBrain.gameArray[1][2])"
+        cell13.text = "\(gameBrain.gameArray[1][3])"
+        cell20.text = "\(gameBrain.gameArray[2][0])"
+        cell21.text = "\(gameBrain.gameArray[2][1])"
+        cell22.text = "\(gameBrain.gameArray[2][2])"
+        cell23.text = "\(gameBrain.gameArray[2][3])"
+        cell30.text = "\(gameBrain.gameArray[3][0])"
+        cell31.text = "\(gameBrain.gameArray[3][1])"
+        cell32.text = "\(gameBrain.gameArray[3][2])"
+        cell33.text = "\(gameBrain.gameArray[3][3])"
+        labelScore.text = "\(gameBrain.score)"
+        // Check if game is over
+        if gameBrain.checkGameOver() == true {
+            labelGameOver.isHidden = false
+            buttonNewGame.isHidden = false
+            swipeUp.isEnabled = false
+            swipeDown.isEnabled = false
+            swipeLeft.isEnabled = false
+            swipeRight.isEnabled = false
+        }
+    }
+    
+    @IBAction func swipedRight(_ sender: UISwipeGestureRecognizer) {
+        print("Swiped Right!")
+        var i = 0
+        gameBrain.pointerArray = [3, 3, 3, 3]
+        repeat {
+            gameBrain.moveRight()
+            i += 1
+        } while i < 3
+        gameBrain.continueGame()
+        updateCells()
+    }
+    
+    @IBAction func swipedLeft(_ sender: UISwipeGestureRecognizer) {
+        print("Swiped Left!")
+        var i = 0
+        gameBrain.pointerArray = [0, 0, 0, 0]
+        repeat {
+            gameBrain.moveLeft()
+            i += 1
+        } while i < 3
+        gameBrain.continueGame()
+        updateCells()
+    }
+    
+    @IBAction func swipedDown(_ sender: UISwipeGestureRecognizer) {
+        print("Swiped Down!")
+        var i = 0
+        gameBrain.pointerArray = [3, 3, 3, 3]
+        repeat {
+            gameBrain.moveDown()
+            i += 1
+        } while i < 3
+        gameBrain.continueGame()
+        updateCells()
+    }
+    
+    @IBAction func swipedUp(_ sender: UISwipeGestureRecognizer) {
+        print("Swiped Up!")
+        var i = 0
+        gameBrain.pointerArray = [0, 0, 0, 0]
+        repeat {
+            gameBrain.moveUp()
+            i += 1
+        } while i < 3
+        gameBrain.continueGame()
+        updateCells()
+    }
+    
+    @IBAction func buttonNewGamePressed(_ sender: UIButton) {
+        gameBrain.startNewGame()
+        labelGameOver.isHidden = true
+        buttonNewGame.isHidden = true
+        swipeUp.isEnabled = true
+        swipeDown.isEnabled = true
+        swipeLeft.isEnabled = true
+        swipeRight.isEnabled = true
+        updateCells()
+    }
+}
+
